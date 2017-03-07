1b7cb65805bd9c3c69dc0e67a16fab32dc87c439
Miguel Rivas <rivasmig@gmail.com>
2017-03-07 16:49:44 +0000

    update app.js, index.html

--- app.js
+++ app.js
@@ -11810,21 +11810,21 @@ var _jquery2 = _interopRequireDefault(_jquery);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /*              API WEATHER               */
-var IMG_WEATHER = "http://openweathermap.org/img/w/";
+var IMG_WEATHER = "https://openweathermap.org/img/w/";
 
 var templateWeather = '<article class="weather-container">' + '<div class="weather-img">' + '<img src=":img:" alt=":img alt:" />' + '</div>' + '<div class="weather-info">' + '<p>:name:, :country: - :temperature: °C</p>' +
 /*'<p>:description:</p>' +
 '<p>:summary:</p>' +*/
 '</div>' + '</article>';
 
-_jquery2.default.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Santa%20Lucia,%20ES&appid=0797f636000a85080848139a78cccf75', getCurrentWeather);
+_jquery2.default.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Santa%20Lucia,%20ES&appid=0797f636000a85080848139a78cccf75', getCurrentWeather);
 
 function getCurrentWeather(data) {
   console.log(data);
   console.log(data.name);
   var celciusTemp = data.main.temp - 273.15;
   var tempCurrent = celciusTemp.toFixed(1);
-  var article = templateWeather.replace(':name:', data.name).replace(':country:', data.sys.country).replace(':img:', "http://openweathermap.org/img/w/" + data.weather["0"].icon + ".png").replace(':img alt:', data.name)
+  var article = templateWeather.replace(':name:', data.name).replace(':country:', data.sys.country).replace(':img:', "https://openweathermap.org/img/w/" + data.weather["0"].icon + ".png").replace(':img alt:', data.name)
   /*.replace(':description:', data.weather["0"].description)
   .replace(':summary:', data.weather["0"].main)*/
   .replace(':temperature:', tempCurrent);
